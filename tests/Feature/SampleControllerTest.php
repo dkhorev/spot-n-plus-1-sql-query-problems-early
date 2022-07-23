9047c17142875d9cf7b29bd53e78fd30810fa90b
dkhorev <me@dkhorev.pro>
2022-07-23 15:30:59 -0400

    improve test cases file

--- tests/Feature/SampleControllerTest.php
+++ tests/Feature/SampleControllerTest.php
@@ -17,14 +17,11 @@ class SampleControllerTest extends TestCase
     public function last100_debugbar_when_correct_requst_then_has_expected_resource_structure(): void
     {
         $this->seed(DatabaseSeeder::class);
-        $debugbar = new LaravelDebugbar();
-        $debugbar->boot();
 
         // act
         $response = $this->getJson(route('api.last100'));
 
         // assert
-        $queryCount = count($debugbar->collect()['queries']['statements']);
         $response->assertJsonStructure([
             'data' => [
                 [
@@ -35,11 +32,25 @@ class SampleControllerTest extends TestCase
                 ],
             ],
         ]);
+    }
+
+    /** @test */
+    public function last100_debugbar_when_correct_requst_then_has_expected_query_count(): void
+    {
+        $this->seed(DatabaseSeeder::class);
+        $debugbar = new LaravelDebugbar();
+        $debugbar->boot();
+
+        // act
+        $response = $this->getJson(route('api.last100'));
+
+        // assert
+        $queryCount = count($debugbar->collect()['queries']['statements']);
         $this->assertSame(2, $queryCount);
     }
 
     /** @test */
-    public function last100_telescope_when_correct_requst_then_has_expected_resource_structure(): void
+    public function last100_telescope_when_correct_requst_then_has_expected_query_count(): void
     {
         $this->seed(DatabaseSeeder::class);
         /** @var EntriesRepository $storage */
@@ -56,16 +67,27 @@ class SampleControllerTest extends TestCase
         // finds all queries executed in SampleResource file
         $queryCount = $entries->filter(fn($e) => str_contains($e->content['file'], 'SampleResource'))
             ->count();
-        $response->assertJsonStructure([
-            'data' => [
-                [
-                    'temp',
-                    'hardware_id',
-                    'location',
-                    'datetime',
-                ],
-            ],
-        ]);
+        $this->assertSame(0, $queryCount);
+    }
+
+    /** @test */
+    public function last100_dbquerylog_when_correct_requst_then_has_expected_query_count(): void
+    {
+        $this->seed(DatabaseSeeder::class);
+        /** @var EntriesRepository $storage */
+        $storage = resolve(EntriesRepository::class);
+
+        // act
+        $response = $this->getJson(route('api.last100'));
+
+        // assert
+        $entries = $storage->get(
+            EntryType::QUERY,
+            (new EntryQueryOptions())->limit(100)
+        );
+        // finds all queries executed in SampleResource file
+        $queryCount = $entries->filter(fn($e) => str_contains($e->content['file'], 'SampleResource'))
+            ->count();
         $this->assertSame(0, $queryCount);
     }
 }
